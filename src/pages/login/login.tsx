ffd3be59775b50ec7fda03f61ffa7159c375ecb1
Lakshya Mittal <lakshyamittalaka@gmail.com>
2025-08-15 03:49:58 +0530

    First component

---
+++ src/pages/login/login.tsx
@@ -0,0 +1,15 @@
+const LoginPage = () => {
+    return (
+        <>
+            <h1>Sign in</h1>;
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button>Log in</button>
+            <label htmlFor="remember-me">Remember me</label>
+            <input id="remember-me" type="checkbox" />
+            <a href="#">Forgot password</a>
+        </>
+    );
+};
+
+export default LoginPage;
