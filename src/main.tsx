d5231bcd173eb18822dd93881761e1ff9ff8a979
Lakshya Mittal <lakshyamittalaka@gmail.com>
2025-08-16 02:54:25 +0530

    Make login page

--- src/main.tsx
+++ src/main.tsx
@@ -6,17 +6,22 @@ import "antd/dist/reset.css";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <ConfigProvider
-            theme={{
-                token: {
-                    colorPrimary: "#F65F42",
-                },
-            }}
-        >
-            <RouterProvider router={router} />
-        </ConfigProvider>
+        <QueryClientProvider client={queryClient}>
+            <ConfigProvider
+                theme={{
+                    token: {
+                        colorPrimary: "#F65F42",
+                    },
+                }}
+            >
+                <RouterProvider router={router} />
+            </ConfigProvider>
+        </QueryClientProvider>
     </StrictMode>
 );
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,9 +1,26 @@
-import { Flex, Checkbox, Input, Layout, Card, Space, Form, Button } from "antd";
+import { Flex, Checkbox, Input, Layout, Card, Space, Form, Button, Alert } from "antd";
 import { Content } from "antd/es/layout/layout";
 import { LockFilled, UserOutlined, LockOutlined } from "@ant-design/icons";
 import Logo from "../../components/icons/Logo";
+import { useMutation } from "@tanstack/react-query";
+import type { Credentials } from "../../types";
+import { login } from "../../http/api";
+
+const loginUser = async (credentials: Credentials) => {
+    // Server call logic
+    const { data } = await login(credentials);
+    return data;
+};
 
 const LoginPage = () => {
+    const { mutate, isPending, isError, error } = useMutation({
+        mutationKey: ["login"],
+        mutationFn: loginUser,
+        onSuccess: async () => {
+            console.log("Login Successful");
+        },
+    });
+
     return (
         <>
             <Layout style={{ minHeight: "100vh" }}>
@@ -27,7 +44,15 @@ const LoginPage = () => {
                             style={{ width: 300 }}
                             styles={{ title: { textAlign: "center" } }}
                         >
-                            <Form name="login-form" initialValues={{ remember: true }}>
+                            <Form
+                                name="login-form"
+                                initialValues={{ remember: true }}
+                                onFinish={(values) => {
+                                    mutate({ email: values.email, password: values.password });
+                                    console.log(values);
+                                }}
+                            >
+                                {isError && <Alert type="error" message={error.message} />}
                                 <Form.Item
                                     name="email"
                                     rules={[
@@ -47,7 +72,6 @@ const LoginPage = () => {
                                         placeholder="Email"
                                     />
                                 </Form.Item>
-
                                 <Form.Item
                                     name="password"
                                     rules={[
@@ -62,7 +86,6 @@ const LoginPage = () => {
                                         placeholder="Password"
                                     />
                                 </Form.Item>
-
                                 <Form.Item>
                                     <Flex justify="space-between" align="center">
                                         <Form.Item name="remember" noStyle valuePropName="checked">
@@ -71,9 +94,13 @@ const LoginPage = () => {
                                         <a href="">Forgot password</a>
                                     </Flex>
                                 </Form.Item>
-
                                 <Form.Item>
-                                    <Button block type="primary" htmlType="submit">
+                                    <Button
+                                        block
+                                        type="primary"
+                                        htmlType="submit"
+                                        loading={isPending}
+                                    >
                                         Log in
                                     </Button>
                                 </Form.Item>
