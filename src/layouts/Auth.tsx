483efb9a1f69a781b065651f0c8bc6074c11213f
Lakshya Mittal <lakshyamittalaka@gmail.com>
2025-08-17 18:12:55 +0530

    Add protected routes

--- src/layouts/Auth.tsx
+++ src/layouts/Auth.tsx
@@ -1,6 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router";
+import { useAuthStore } from "../store";
 
 const Auth = () => {
+    const { user } = useAuthStore();
+
+    if (user !== null) {
+        return <Navigate to="/" replace={true} />;
+    }
+
     return (
         <div>
             <h1>Auth</h1>
